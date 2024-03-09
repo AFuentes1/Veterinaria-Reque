bd7daf50ca368caf5da46cd30b39bbab1a56cd90
Anthony Fuentes <84689442+AFuentes1@users.noreply.github.com>
2024-03-08 23:17:42 -0600

    [ADD] Archivos

---
+++ FrontEnd Reque/FrontEnd.js
@@ -0,0 +1,13 @@
+document.getElementById("cambiarColor").addEventListener("click", function() {
+    var color = getRandomColor();
+    document.body.style.backgroundColor = color;
+});
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
