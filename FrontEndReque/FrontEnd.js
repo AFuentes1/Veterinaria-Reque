493f900d7560338426ebfa2109fba45dfc253b6a
AFuentes1 <anthonyfuentescalvo@gmail.com>
2024-03-09 18:22:39 -0600

    [UPDATE] Se agregan Funcionalidades a los botones de Inicio

--- FrontEndReque/FrontEnd.js
+++ FrontEndReque/FrontEnd.js
@@ -1,13 +1,50 @@
-document.getElementById("CambiarColor").addEventListener("click", function() {
-    var color = getRandomColor();
-    document.body.style.backgroundColor = color;
-});
+document.addEventListener('DOMContentLoaded', function() {
+    //Obtener elementos necesarios del DOM
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById("openModalLink");
+    var span = document.getElementsByClassName("close")[0];
 
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+    //Funciones para abrir y cerrar el modal
+    btn.onclick = function() {
+        modal.style.display = "block";
     }
-    return color;
+
+    //cuando el usuario haga click en la X, cerrar el modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    //cuando el usuario haga click fuera del modal, cerrar el modal
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    }
+);
+
+document.addEventListener('DOMContentLoaded', function() {
+    //Obtener elementos necesarios del DOM
+    var modalAcercaDe = document.getElementById('myModalAcercaDe');
+    var btnAcercaDe = document.getElementById("openModalLinkAcercaDe");
+    var spanAcercaDe = document.getElementsByClassName("closeAcercaDe")[0];
+
+    //Funciones para abrir y cerrar el modal
+    btnAcercaDe.onclick = function() {
+        modalAcercaDe.style.display = "block";
+    }
+
+    //cuando el usuario haga click en la X, cerrar el modal
+    spanAcercaDe.onclick = function() {
+        modalAcercaDe.style.display = "none";
+    }
+
+    //cuando el usuario haga click fuera del modal, cerrar el modal
+    window.addEventListener('click', function(event) {
+        if (event.target == modalAcercaDe) {
+            modalAcercaDe.style.display = "none";
+        }
+    });
 }
+
+);
